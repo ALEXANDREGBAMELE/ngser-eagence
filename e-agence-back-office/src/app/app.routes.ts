50e14560d232d329542aea03cbc3b81dcde009b1
alexandre <gbamelealexandre@gmail.com>
2024-04-15 10:04:45 +0000

    last commit

--- e-agence-back-office/src/app/app.routes.ts
+++ e-agence-back-office/src/app/app.routes.ts
@@ -2,20 +2,35 @@ import { Routes } from '@angular/router';
 import { IndexComponent } from './core/pages/index/index.component';
 import { UsersComponent } from './core/pages/users/users.component';
 import { LoginComponent } from './auth/pages/login/login.component';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
     {
         path: 'login',
         component: LoginComponent
     },
     {
-        path: "index",
+        path: 'index',
         component: IndexComponent,
         children: [
             {
                 path: '',
-                component: UsersComponent,
+                redirectTo: 'users',
+                pathMatch: 'full'
+            },
+            {
+                path: 'users',
+                component: UsersComponent
             }
         ]
     },
+    {
+        path: '**', // wildcard route for any other path
+        component: NotFoundComponent
+    }
 ];
